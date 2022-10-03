83cb36b8dacea82a9e7cbb59fbdbabac9c351af9
Ahmet Kalender <kalenderahmet93@outlook.com>
2022-10-03 16:09:50 +0300

    Session kodları yazıldı.

---
+++ WebApplication1/Controllers/SessionDemoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Entities;
+using WebApplication1.Extensions;
+
+namespace WebApplication1.Controllers
+{
+    public class SessionDemoController : Controller
+    {
+        public string Index1()
+        {
+            Employee employee = new Employee { ID = 1, FirstName = "ahmet",LastName="kalender",CityId = 80 };
+
+            HttpContext.Session.SetString("isim", "Ahmet");
+            HttpContext.Session.SetObject("musteri", employee);
+            return "session gönderildi.";
+        }
+
+        public string Index2()
+        {
+            //return HttpContext.Session.GetString("isim");
+            var res = HttpContext.Session.GetObject<Employee>("musteri");
+            return res.FirstName;
+        }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -28,6 +28,8 @@ namespace WebApplication1
             services.AddMvc();
 
             services.AddSingleton<ILogger, DataBaseLogger>();  //Dependency Injection
+
+            services.AddSession(); //KSoft Session
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +56,8 @@ namespace WebApplication1
 
             app.UseHttpsRedirection();
             app.UseStaticFiles(); // Bu bizim wwwroot daki bootstrap jquery kütüphanesini kullanmamýzý saðlar.
+
+            app.UseSession(); //KSoft Session
 
             app.UseRouting();
 
